23378af396b8e774b06e634b41274b2b71c95ff3
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2021-04-05 05:25:09 +0300

    List of the packages

---
+++ packages.sh
@@ -0,0 +1,22 @@
+### AUR helper
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+
+### Key bindings
+sudo pacman -S xbindkeys
+
+
+### Bluetooth
+sudo pacman -S bluez bluez-utils
+
+
+### Sound
+# Mixer
+sudo pacman -S amixer
+
+# Volume notifications
+paru -S volnoti
+
+
