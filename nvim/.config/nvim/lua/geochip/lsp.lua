3632e358f2f12f1e4a23e6eaf7195879a3d5451d
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2024-05-09 19:24:11 +0300

    nvim: update lsp config

--- nvim/.config/nvim/lua/geochip/lsp.lua
+++ nvim/.config/nvim/lua/geochip/lsp.lua
@@ -1,34 +1,24 @@
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local opts = { noremap = true }
+  local opts = { noremap = true, silent = true }
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ld', ':lua vim.lsp.buf.definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>li', ':lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lr', ':lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lh', ':lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ls', ':lua vim.lsp.buf.signature_help()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ln', ':lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>la', ':lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lf', ':lua vim.lsp.buf.format()<CR>', opts)
 end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- linter and formatter
---local null_ls = require("null-ls")
---null_ls.setup({
-    --sources = {
-        --null_ls.builtins.formatting.stylua,
-        --null_ls.builtins.diagnostics.eslint,
-        --null_ls.builtins.completion.spell,
-        --null_ls.builtins.diagnostics.mypy,
-        --null_ls.builtins.diagnostics.pycodestyle,
-    --},
---})
-
 -- Enable the following language servers
 local lspconfig = require('lspconfig')
-local servers = { 'clangd', 'pyright', 'rust_analyzer', 'zls', 'gopls', 'bashls' }
+local servers = { 'lua_ls', 'clangd', 'pyright', 'rust_analyzer', 'zls', 'gopls', 'bashls', 'intelephense', 'lemminx', 'tsserver', 'java_language_server' }
 for _, server in ipairs(servers) do
   lspconfig[server].setup({
     on_attach = on_attach,
