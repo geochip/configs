a43e74659a28aa5118e1136159516a8642c10a95
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2024-05-14 06:46:48 +0300

    nvim(nvim-cmp): do like in the docs

--- nvim/.config/nvim/lua/geochip/nvim-cmp.lua
+++ nvim/.config/nvim/lua/geochip/nvim-cmp.lua
@@ -13,7 +13,7 @@ cmp.setup({
 
   window = {},
 
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -24,7 +24,7 @@ cmp.setup({
     }),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
-  },
+  }),
 
   sources = cmp.config.sources({
       { name = 'nvim_lsp' },
