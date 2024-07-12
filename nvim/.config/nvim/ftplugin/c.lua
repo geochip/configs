62c6e8110fa91a2ace61bcfa8a0790e02d4b4938
Alexander Stepchenko <geochip@altlinux.org>
2024-07-12 16:59:36 +0300

    nvim: change indent options for c and cpp

--- nvim/.config/nvim/ftplugin/c.lua
+++ nvim/.config/nvim/ftplugin/c.lua
@@ -3,3 +3,4 @@ vim.bo.tabstop = indent
 vim.bo.softtabstop = indent
 vim.bo.shiftwidth = indent
 vim.bo.expandtab = true
+vim.bo.cinoptions = 'l1'
--- nvim/.config/nvim/ftplugin/cpp.lua
+++ nvim/.config/nvim/ftplugin/cpp.lua
@@ -3,4 +3,5 @@ vim.bo.tabstop = indent
 vim.bo.softtabstop = indent
 vim.bo.shiftwidth = indent
 vim.bo.expandtab = true
+vim.bo.cinoptions = 'l1'
 --vim.bo.autoindent = true
