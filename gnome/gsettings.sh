74ca7e4fcf132683f77d079665d9996b2567e5a4
Alexander Stepchenko <x.geochip.x@yandex.ru>
2024-08-20 23:31:19 +0300

    gnome: add workspaces settings

--- gnome/gsettings.sh
+++ gnome/gsettings.sh
@@ -25,6 +25,9 @@ for i in {1..9}; do
 	gsettings set org.gnome.shell.keybindings switch-to-application-$i "[]"
 done
 
+gsettings set org.gnome.desktop.wm.preferences num-workspaces '9'
+gsettings set org.gnome.mutter dynamic-workspaces 'false'
+
 for i in {1..9}; do
 	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
 done
