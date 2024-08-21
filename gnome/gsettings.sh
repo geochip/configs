e8e05ebe1cb04c3a2066af1b9adfdaa08777a0c4
Alexander Stepchenko <x.geochip.x@yandex.ru>
2024-08-21 07:37:08 +0300

    gnome: limit switcher to current workspace

--- gnome/gsettings.sh
+++ gnome/gsettings.sh
@@ -27,6 +27,7 @@ done
 
 gsettings set org.gnome.desktop.wm.preferences num-workspaces '9'
 gsettings set org.gnome.mutter dynamic-workspaces 'false'
+gsettings set org.gnome.shell.app-switcher current-workspace-only 'true'
 
 for i in {1..9}; do
 	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
