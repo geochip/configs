b74ff7b7e1731dd84b1b08f60b60f221c7218146
Alexander Stepchenko <x.geochip.x@yandex.ru>
2024-08-21 20:36:09 +0300

    fix the copypasta

--- install_fonts.sh
+++ install_fonts.sh
@@ -7,11 +7,14 @@ if [ ! -d "$FONTS_DIR" ]; then
 fi
 
 roboto_mono_file="$HOME/Downloads/RobotoMono.zip"
-[ -f RobotoMono.zip ] \
+[ -f "$roboto_mono_file" ] \
 	|| wget -O "$roboto_mono_file" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
 unzip -d "$FONTS_DIR/RobotoMono" "$roboto_mono_file"
 
-jetbrains_mono_file="$HOME/Downloads/RobotoMono.zip"
-[ -f JetBrainsMono.zip ] \
+jetbrains_mono_file="$HOME/Downloads/JetBrainsMono.zip"
+[ -f "$jetbrains_mono_file" ] \
 	|| wget -O "$jetbrains_mono_file" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip -d "$FONTS_DIR/JetBrainsMono" "$jetbrains_mono_file"
+
+rm "$roboto_mono_file"
+rm "$jetbrains_mono_file"
