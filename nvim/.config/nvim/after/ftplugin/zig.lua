f65bbd2cfa3c28b3217959e608b6be6def9004b4
Alexander Stepchenko <x.geochip.x@yandex.ru>
2025-02-05 10:00:18 +0300

    nvim: update ftplugin

--- nvim/.config/nvim/after/ftplugin/zig.lua
+++ nvim/.config/nvim/after/ftplugin/zig.lua
@@ -2,4 +2,5 @@ local indent = 4
 vim.bo.tabstop = indent
 vim.bo.softtabstop = indent
 vim.bo.shiftwidth = indent
-vim.bo.expandtab = false
+vim.bo.expandtab = true
+vim.opt.formatoptions:remove({'r', 'o'})
